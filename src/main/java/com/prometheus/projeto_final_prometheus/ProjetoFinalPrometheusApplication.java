ab914a6b15d0ed882cae450f97a1b4a18e8ea8cd
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-09-06 16:44:47 -0300

    doc: criando spring boot

---
+++ src/main/java/com/prometheus/projeto_final_prometheus/ProjetoFinalPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.prometheus.projeto_final_prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalPrometheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prometheus/projeto_final_prometheus/ProjetoFinalPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometheus.projeto_final_prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
