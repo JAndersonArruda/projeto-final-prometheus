c9e9b416f36cff6c2a815e511d66248f9f0a45db
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-13 13:09:10 -0300

    refactor: separando os array com dados test em diretorio proprio

--- eventos-app/src/pages/Home/Home.tsx
+++ eventos-app/src/pages/Home/Home.tsx
@@ -5,48 +5,16 @@ import "./home.css"
 import Header from "../../components/Header/Header";
 import CardEvent from "../../components/Card/CardEvent"
 
-export default function Home() {
-    let contID = 1;
-    const arrayTest = [
-        {
-            id: contID ++,
-            title: "Projeto POO",
-            image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
-            dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-            localEvent: "IFPB - Cajazeiras, Lab. 4"
-        },
-        {
-            id: contID ++,
-            title: "Projeto POO",
-            image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
-            dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-            localEvent: "IFPB - Cajazeiras, Lab. 4"
-        },
-        {
-            id: contID ++,
-            title: "Projeto POO",
-            image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
-            dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-            localEvent: "IFPB - Cajazeiras, Lab. 4"
-        },
-        {
-            id: contID ++,
-            title: "Projeto POO",
-            image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
-            dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-            localEvent: "IFPB - Cajazeiras, Lab. 4"
-        }
-    ];
+//Utilizado para testes
+import { ArrayTest } from "../../service/ArrayDados";
 
+export default function Home() {
     return (
         <>
             <Header />
             <div className="container-home">
-                <h1>Home</h1>
-                <p>Bem-vindo ao site!</p>
-                <p>Aqui você pode ver eventos, se inscrever, verificar seu status, etc.</p>
                 <div className="container-cards-event">
-                    {arrayTest.map(eventDate => 
+                    {ArrayTest.map(eventDate => 
                         <CardEvent 
                             key={eventDate.id}
                             id={eventDate.id}
@@ -59,6 +27,5 @@ export default function Home() {
                 </div>
             </div>
         </>
-        
     )
 }
---
+++ eventos-app/src/service/ArrayDados.tsx
@@ -0,0 +1,164 @@
+let contID = 1;
+export const ArrayTest = [
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    },
+    {
+        id: contID ++,
+        title: "Projeto POO",
+        image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
+        dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
+        localEvent: "IFPB - Cajazeiras, Lab. 4"
+    }
+];
