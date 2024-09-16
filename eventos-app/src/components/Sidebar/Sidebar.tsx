dcdafc16c62d97594beb72847c6a128ff7d6db5d
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-16 14:25:21 -0300

    refactor: atualizando as rotas de accounts

--- eventos-app/src/components/Sidebar/Sidebar.tsx
+++ eventos-app/src/components/Sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ function Sidebar() {
     return (
         <ul className='contianer-sidebar-elements'>
             <li><Link to={"/"}>Home</Link></li>
-            <li><Link to={"/login"}>Login</Link></li>
+            <li><Link to={"/account/login"}>Login</Link></li>
             <li><Link to={"/profile"}>Perfil</Link></li>
             <li><Link to={"/ticket-event"}>Inscrição</Link></li>
             <li><Link to={"/events/view"}>View Eventos</Link></li>
--- eventos-app/src/routes/index.tsx
+++ eventos-app/src/routes/index.tsx
@@ -2,7 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Home from '../pages/Home/Home'
-import Login from '../pages/Login/Login'
+import Account from '../pages/Account/Account'
 import Profile from '../pages/Profile/Profile'
 import TicketEvent from '../pages/TicketEvent/TicketEvent'
 import ViewEvent from '../pages/ViewEvent/ViewEvent'
@@ -15,7 +15,8 @@ const AppRoutes = () => {
             <Route path="/events/criate" element={<Home />} />
             <Route path="/events/edit/:id" element={<Home />} />
             <Route path="/events/view" element={<ViewEvent />} />
-            <Route path="/login" element={<Login /> } />
+            <Route path="/account/login" element={<Account /> } />
+            <Route path="/account/register" element={<Account />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/ticket-event" element={<TicketEvent />} />
             <Route path="*" element={<h1>Not Found</h1>} />
