418a44dbaa60d694f6982f1f95539018e9574dc3
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-16 15:23:33 -0300

    fix: estruturando rota defalt

--- eventos-app/src/routes/index.tsx
+++ eventos-app/src/routes/index.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import Home from '../pages/Home/Home'
 import Account from '../pages/Account/Account'
@@ -11,14 +11,16 @@ import ViewEvent from '../pages/ViewEvent/ViewEvent'
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Navigate to={"/account/login"} />} />
+
+            <Route path="/account/login" element={<Account /> } />
+            <Route path="/account/register" element={<Account />} />
+            <Route path="/events" element={<Home />} />
             <Route path="/events/criate" element={<Home />} />
             <Route path="/events/edit/:id" element={<Home />} />
             <Route path="/events/view" element={<ViewEvent />} />
-            <Route path="/account/login" element={<Account /> } />
-            <Route path="/account/register" element={<Account />} />
-            <Route path="/profile" element={<Profile />} />
             <Route path="/ticket-event" element={<TicketEvent />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
     )
