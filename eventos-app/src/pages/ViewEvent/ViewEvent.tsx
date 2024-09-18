69336ce9bb915969e0ae6c1359eebb334a08d02d
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-18 11:14:39 -0300

    feat: preenchimento com os dados teste

--- eventos-app/src/pages/ViewEvent/ViewEvent.tsx
+++ eventos-app/src/pages/ViewEvent/ViewEvent.tsx
@@ -2,25 +2,39 @@
 
 import "./viewEvent.css"
 
+import { ArrayTest } from "../../service/ArrayDados"
+
 export default function ViewEvent() {
+    const pathID = 1;
+    const pathEvent = ArrayTest.find(element => element.id === pathID);
+
+    const dateEvent = pathEvent?.dateTime.toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
+    const timeEvent = pathEvent?.dateTime.toLocaleTimeString([], {
+        hour: "2-digit", 
+        minute: "2-digit"
+    });
+
     return (
         <>
             <div id="container-page" className="container-view-event">
                 <div className="content-image">
-                    <img src="" alt="imagem do evento" />
+                    <img src={pathEvent?.image} alt="imagem do evento" />
                 </div>
                 <div className="content-title">
-                    <h2>Título da Imagem</h2>
+                    <h2>{pathEvent?.title}</h2>
                 </div>
                 <div className="content-data">
-                    <p className="location">Local</p>
-                    <p className="date-event">Data<span className="time-event">Hora</span></p>
+                    <p className="location">{pathEvent?.localEvent}</p>
+                    <p className="time-event">{timeEvent} <span className="date-event">{dateEvent}</span></p>
                 </div>
                 <div className="content-description">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque itaque velit a impedit aliquid, deleniti veritatis perferendis repellendus beatae rerum rem molestiae nobis, iure vero molestias dolorem illo maiores.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident dolor doloremque earum voluptas sed quis necessitatibus laudantium doloribus minima aut, debitis accusamus atque optio, eum voluptatum sequi sapiente animi ab.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nesciunt adipisci corrupti impedit amet iure ex error animi blanditiis qui, perspiciatis rerum nulla! Aspernatur sint porro veniam ipsam, eaque suscipit!</p>
+                    {pathEvent?.description?.split("\n").map((paragraph, index) => (
+                        <p key={index}>{paragraph}</p>
+                    ))}
                 </div>
                 <button className="button-ticket">Realizar Inscrição</button>
             </div>
--- eventos-app/src/service/ArrayDados.tsx
+++ eventos-app/src/service/ArrayDados.tsx
@@ -5,21 +5,24 @@ export const ArrayTest = [
         title: "Projeto POO",
         image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
         dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-        localEvent: "IFPB - Cajazeiras, Lab. 4"
+        localEvent: "IFPB - Cajazeiras, Lab. 4",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo? \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?\nLorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?"
     },
     {
         id: contID ++,
         title: "Projeto POO",
         image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
         dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-        localEvent: "IFPB - Cajazeiras, Lab. 4"
+        localEvent: "IFPB - Cajazeiras, Lab. 4",
+         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo? \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?\nLorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?"
     },
     {
         id: contID ++,
         title: "Projeto POO",
         image: "https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/crop/capa/2021/07/evento.png",
         dateTime: new Date(2024, 8, 23, 9, 0), // class Date salva indice do mes 0 - 11. exp.: indice 8 = mes 9
-        localEvent: "IFPB - Cajazeiras, Lab. 4"
+        localEvent: "IFPB - Cajazeiras, Lab. 4",
+         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo? \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?\nLorem ipsum dolor sit amet consectetur adipisicing elit. Optio corporis ad officia. Sint ratione omnis voluptate modi repellendus excepturi, enim sed aliquid rerum fugiat voluptatum qui saepe? Recusandae, natus illo?"
     },
     {
         id: contID ++,
