e82bf0e95bde930854b11457e1a82b4c662f15ae
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-19 22:30:04 -0300

    view: finalizando estilização estatica do perfil

--- eventos-app/src/pages/Profile/Profile.tsx
+++ eventos-app/src/pages/Profile/Profile.tsx
@@ -13,45 +13,51 @@ export default function Profile() {
             <div id="container-page" className="container-profile">
                 <div className="area-profile">
                     <div className="content-data-personal">
-                        <div className='data-user'>
-                            <img className="image-prifile" src="https://i.pinimg.com/564x/57/00/c0/5700c04197ee9a4372a35ef16eb78f4e.jpg" alt="user-image" />
-                        </div>
-                        <div className='data-account'>
-                            <h3>UserName</h3>
-                            <p>Nome do Usuário</p>
-                            <p>E-mail: </p>
+                        <h4 className="title-section-personal">Dados Pessoais</h4>
+                        <div className="personal">
+                            <div className='data-image-user'>
+                                <img className="image-prifile" src="https://i.pinimg.com/564x/57/00/c0/5700c04197ee9a4372a35ef16eb78f4e.jpg" alt="user-image" />
+                            </div>
+                            <div className='data-account'>
+                                <p id="mark-1">
+                                    <span className="content-date-profile">Username: <span className="body-date-profile">corpo do username</span></span>
+                                    <span className="content-date-profile">E-mail: <span className="body-date-profile">corpo do email</span></span>
+                                </p>
+                                <p id="mark-2">
+                                    <span className="content-date-profile">Nome: <span className="body-date-profile">corpo do nome</span></span>
+                                    <span className="content-date-profile">Tipo: <span className="body-date-profile">corpo do tipo</span></span>
+                                </p>
+                            </div>
                         </div>
                     </div>
                     <div className="content-data-certificate">
-                        <div>
-                            <h3>Certificados</h3>
-                            <div className="certificate">
-                                <CertificateItem
-                                    name={"Certificado 00371291173"}
-                                    date={"20/10/2024"}
-                                    timeLine={"3:00"}
-                                />
-                                <CertificateItem
-                                    name={"Certificado 00371291173"}
-                                    date={"20/10/2024"}
-                                    timeLine={"3:00"}
-                                />
-                                <CertificateItem
-                                    name={"Certificado 00371291173"}
-                                    date={"20/10/2024"}
-                                    timeLine={"3:00"}
-                                />
-                                <CertificateItem
-                                    name={"Certificado 00371291173"}
-                                    date={"20/10/2024"}
-                                    timeLine={"3:00"}
-                                />
-                                <CertificateItem
-                                    name={"Certificado 00371291173"}
-                                    date={"20/10/2024"}
-                                    timeLine={"3:00"}
-                                />
-                            </div>
+                        <h4 className="title-section-certificate">Certificados</h4>
+                        <div className="certificate">
+                            <CertificateItem
+                                name={"Certificado 00371291173"}
+                                date={"20/10/2024"}
+                                timeLine={"3:00"}
+                            />
+                            <CertificateItem
+                                name={"Certificado 00371291173"}
+                                date={"20/10/2024"}
+                                timeLine={"3:00"}
+                            />
+                            <CertificateItem
+                                name={"Certificado 00371291173"}
+                                date={"20/10/2024"}
+                                timeLine={"3:00"}
+                            />
+                            <CertificateItem
+                                name={"Certificado 00371291173"}
+                                date={"20/10/2024"}
+                                timeLine={"3:00"}
+                            />
+                            <CertificateItem
+                                name={"Certificado 00371291173"}
+                                date={"20/10/2024"}
+                                timeLine={"3:00"}
+                            />
                         </div>
                     </div>
                 </div>
