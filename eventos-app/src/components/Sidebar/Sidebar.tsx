d8a80a7569b0a60f1b8efaf292681325d51fc8de
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-20 02:30:33 -0300

    doc: configurando biblioteca de icones

--- eventos-app/src/components/Sidebar/Sidebar.tsx
+++ eventos-app/src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,20 @@
 // import React from 'react'
 import { Link } from 'react-router-dom'
+import { Home, Login, AccountCircle, Event, CreateOutlined, AddCircle, EditCalendar, DeleteOutlineOutlined } from '@mui/icons-material'
 
 import "./sidebar.css"
 
 function Sidebar() {
     return (
         <ul className='contianer-sidebar-elements'>
-            <li><Link to={"/events"}>Home</Link></li>
-            <li><Link to={"/account/login"}>Login</Link></li>
-            <li><Link to={"/profile"}>Perfil</Link></li>
-            <li><Link to={"/ticket-event"}>Inscrição</Link></li>
-            <li><Link to={"/events/view"}>View Eventos</Link></li>
-            <li><Link to={"/events/criate"}>Criar Evento</Link></li>
-            <li><Link to={"/events/edit/1"}>Editar Evento</Link></li>
+            <li><Link to={"/events"}><Home /> Home</Link></li>
+            <li><Link to={"/account/login"}><Login /> Login</Link></li>
+            <li><Link to={"/profile"}><AccountCircle /> Perfil</Link></li>
+            <li><Link to={"/ticket-event"}><EditCalendar /> Inscrição</Link></li>
+            <li><Link to={"/events/view"}><Event /> View Eventos</Link></li>
+            <li><Link to={"/events/criate"}><AddCircle /> Criar Evento</Link></li>
+            <li><Link to={"/events/edit/1"}><CreateOutlined /> Editar Evento</Link></li>
+            <li><Link to={""}><DeleteOutlineOutlined /> Deletar Evento</Link></li>
         </ul>
     )
 }
