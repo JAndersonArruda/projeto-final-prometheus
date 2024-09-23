ecec6e8d3251d9d11e3a1db90e890ebc95f35357
victorivis <knightdragonred@gmail.com>
2024-09-23 20:10:22 -0300

    feat: botao de incricao em evento funcional

--- eventos-app/src/service/eventAPI.ts
+++ eventos-app/src/service/eventAPI.ts
@@ -30,3 +30,46 @@ export const getEventos = async () => {
     }
 };
 
+export async function joinEvent(id: bigint) {
+    const token: string = localStorage.getItem("token") ?? "";
+    const response = await fetch(`${API_BASE_URL}/join`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({ id }),
+    });
+
+    if (!response.ok) {
+        alert("Um erro ocorreu. Não foi possível se registrar nesse evento.");
+        throw new Error("Registro falhou");
+    }
+    else {
+        alert("Registrado no evento com sucesso!");
+    }
+
+    return response.text();
+}
+
+export async function leaveEvent(id: bigint) {
+    const token: string = localStorage.getItem("token") ?? "";
+    const response = await fetch(`${API_BASE_URL}/leave`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({ id }),
+    });
+
+    if (!response.ok) {
+        alert("Um erro ocorreu. Não foi possível cancelar inscrição no evento.");
+        throw new Error("Registro falhou");
+    }
+    else {
+        alert("Registrado no evento com sucesso!");
+    }
+
+    return response.text();
+}
