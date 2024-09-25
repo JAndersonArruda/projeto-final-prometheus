a466cc864e3d6a1ef6339783f61e7e69ac071a0b
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-09-24 21:59:57 -0300

    refactor: ajusatando menu de navegação

--- eventos-app/src/components/Sidebar/Sidebar.tsx
+++ eventos-app/src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import { Link } from 'react-router-dom'
-import { Home, Login, AccountCircle, CreateOutlined, AddCircle, EditCalendar } from '@mui/icons-material'
+import { Home, AccountCircle, AddCircle } from '@mui/icons-material'
 
 import "./sidebar.css"
 
@@ -8,11 +8,9 @@ import "./sidebar.css"
 function Sidebar() {
     return (
         <ul className='contianer-sidebar-elements'>
-            <li><Link to={"/events"}><Home /> Home</Link></li>
             <li><Link to={"/profile"}><AccountCircle /> Perfil</Link></li>
-            <li><Link to={"/ticket-event"}><EditCalendar /> Inscrição</Link></li>
+            <li><Link to={"/events"}><Home /> Home</Link></li>
             <li><Link to={"/events/create"}><AddCircle /> Criar Evento</Link></li>
-            <li><Link to={"/events/edit/1"}><CreateOutlined /> Editar Evento</Link></li>
         </ul>
     )
 }
--- eventos-app/src/routes/index.tsx
+++ eventos-app/src/routes/index.tsx
@@ -4,7 +4,6 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Account from '../pages/Account/Account'
 import Profile from '../pages/Profile/Profile'
-import TicketEvent from '../pages/TicketEvent/TicketEvent'
 import ViewEvent from '../pages/ViewEvent/ViewEvent'
 import CreateEvent from '../pages/CreateEvent/CreateEvent'
 import EditEvent from '../pages/EditEvent/EditEvent'
@@ -21,7 +20,6 @@ const AppRoutes = () => {
             <Route path="/events/create" element={<CreateEvent />} />
             <Route path="/events/edit/:id" element={<EditEvent/>} />
             <Route path="/events/view/:id" element={<ViewEvent />} />
-            <Route path="/ticket-event" element={<TicketEvent />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
